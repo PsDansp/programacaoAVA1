bb429b5d805dc73ce569839d603660a2b80e5e33
PsDansp <danilosp@aluno.ufrb.edu.br>
2023-03-10 18:53:31 -0300

    Adicionada op~l

--- NovoArquivo.c
+++ NovoArquivo.c
@@ -1,41 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (){
 	float liquidificador, barbeador, ventilador, fritadeira, secador, ferro, espremedor, sanduicheira, som,forno;
 	float taxa_liquidificador, taxa_barbeador, taxa_ventilador, taxa_fritadeira, taxa_secador, taxa_ferro, taxa_espremedor, taxa_sanduicheira, taxa_som, taxa_forno;
-	
+	int opcao;
 	
 	printf("Digite o preco do liquidificador Bate Tudo, em seguite insira a taxa de comissao:\n");
 	scanf ("%f %f", &liquidificador, &taxa_liquidificador);
 	
-	printf ("digite o preco do maquina de Barbear seu careca, em seguida insira a taxa de comissao:\n");
-	scanf ("%f %f", &barbeador, &taxa_barbeador);
-	
-	printf ("digite  o preco do ventilador brisa forte, em seguida insira a taxa de comissao:\n");
-	scanf  ("%f %f", &ventilador, &taxa_ventilador);
-	
-	printf ("digite o preco do fritadeira eletrica fogo do sol, em seguida insira a taxa de comissao:\n");
-	scanf  ("%f %f",  &fritadeira, &taxa_fritadeira);
-	
-	printf ("digite o preco do secador de cabelos ventania, em seguida insira a taxa de comissao:\n");
-	scanf  ("%f %f",  &secador, &taxa_secador);
-	
-	printf ("digite o preco do ferro de passar tudo amassado,em seguida insira a taxa de comissao:\n");
-	scanf  ("%f %f",  &ferro, &taxa_ferro);
-	
-	printf ("digite o preco do espremedor de suco so o caldo,em seguida insira a taxa de comissao:\n");
-	scanf  ("%f %f", &espremedor, &taxa_espremedor);
-	
-	printf ("digite o preco do sanduicheira chapa quente,em seguida insira a taxa de comissao:\n");
-	scanf  ("%f %f",  &sanduicheira, &taxa_sanduicheira);
-
-	printf ("digite o preco do caixas de som ouça tudo, em seguida insira a taxa de comissao:\n");
-	scanf  ("%f %f",  &som, &taxa_som);
-	
-	printf ("digite o preco do forno eletrico vulcao,em seguida insira a taxa de comissao:\n");
-	scanf  ("%f %f", &forno, &taxa_forno);
 	
 	
+	system("cls");
 	
+	printf("*******************************************Primeira Compra******************************************\n");
+	printf("Itens:\n\n");
+	printf("1-Liquificador Bate Tudo\n");
+	
+	printf("Opcao> ");
+	scanf("%d", &opcao);
+	printf("\n");
+	
+	switch(opcao){
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+		case 10:
+			break;
+		
+	}
+	
+	
+	
+	return 0;
 	
 }
