46e593fccd375fa736c18949ca42632cbd9646cc
PsDansp <danilosp@aluno.ufrb.edu.br>
2023-03-10 18:28:19 -0300

    Commit inicial

---
+++ NovoArquivo.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main (){
+	float liquidificador, barbeador, ventilador, fritadeira, secador, ferro, espremedor, sanduicheira, som,forno;
+	float taxa_liquidificador, taxa_barbeador, taxa_ventilador, taxa_fritadeira, taxa_secador, taxa_ferro, taxa_espremedor, taxa_sanduicheira, taxa_som, taxa_forno;
+	
+	
+	printf("Digite o preco do liquidificador Bate Tudo, em seguite insira a taxa de comissao:\n");
+	scanf ("%f %f", &liquidificador, &taxa_liquidificador);
+	
+	printf ("digite o preco do maquina de Barbear seu careca, em seguida insira a taxa de comissao:\n");
+	scanf ("%f %f", &barbeador, &taxa_barbeador);
+	
+	printf ("digite  o preco do ventilador brisa forte, em seguida insira a taxa de comissao:\n");
+	scanf  ("%f %f", &ventilador, &taxa_ventilador);
+	
+	printf ("digite o preco do fritadeira eletrica fogo do sol, em seguida insira a taxa de comissao:\n");
+	scanf  ("%f %f",  &fritadeira, &taxa_fritadeira);
+	
+	printf ("digite o preco do secador de cabelos ventania, em seguida insira a taxa de comissao:\n");
+	scanf  ("%f %f",  &secador, &taxa_secador);
+	
+	printf ("digite o preco do ferro de passar tudo amassado,em seguida insira a taxa de comissao:\n");
+	scanf  ("%f %f",  &ferro, &taxa_ferro);
+	
+	printf ("digite o preco do espremedor de suco so o caldo,em seguida insira a taxa de comissao:\n");
+	scanf  ("%f %f", &espremedor, &taxa_espremedor);
+	
+	printf ("digite o preco do sanduicheira chapa quente,em seguida insira a taxa de comissao:\n");
+	scanf  ("%f %f",  &sanduicheira, &taxa_sanduicheira);
+
+	printf ("digite o preco do caixas de som ouça tudo, em seguida insira a taxa de comissao:\n");
+	scanf  ("%f %f",  &som, &taxa_som);
+	
+	printf ("digite o preco do forno eletrico vulcao,em seguida insira a taxa de comissao:\n");
+	scanf  ("%f %f", &forno, &taxa_forno);
+	
+	
+	
+	
+}
